1fa849ad4cc77933e4002876e57bd4b5ee36d74f
Raghadalr02 <123130529+Raghadalr02@users.noreply.github.com>
2023-09-02 15:37:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 import streamlit as st
 from langchain.chains import ConversationChain
-  # Import your API key from a file or set it directly here
+
 from langchain.chains.conversation.memory import ConversationEntityMemory
 from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
 from langchain.llms import OpenAI
-#from langchain.chat_models import ChatOpenAI
 
 # Define a function to suggest a country and its places based on personality using ChatGPT
 def suggest_country_and_places_from_chatgpt(personality):
@@ -52,22 +51,17 @@ Conversation = None
 st.title("✈️ Personalized Trip")
 
 # Assign the API key directly here
-os.environ['OPENAI_API_KEY'] = st.secrets['key']
+os.environ['OPENAI_API_KEY'] = st.secret['key']
 
 # Initialize the OpenAI language model
 llm = OpenAI(temperature=0,
-             openai_api_key=st.secrets['key'],
+             openai_api_key=st.secret['key'],
              model_name='gpt-3.5-turbo',
              verbose=False)
-#llm = ChatOpenAI(temperature=0,
- #           openai_api_key=st.secrets['key'],
-  #           model_name='gpt-3.5-turbo',
-   #          verbose=False
-#)
+
 # Initialize entity memory
 if 'entity_memory' not in st.session_state:
-    st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=st.number_input(
-        ' (#)Summary of prompts to consider', min_value=3, max_value=1000))
+    st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=3)  # Removed the input from the web interface
 
 # Initialize the Conversation object
 Conversation = ConversationChain(
@@ -80,23 +74,19 @@ Conversation = ConversationChain(
 responses = []
 current_question = 0
 
-# Define the new set of 8 questions
+# Define the new set of 4 questions
 questions = [
-    "Do you prefer to focus on concrete details rather than abstract concepts?",
-    "Are you more practical and realistic than imaginative?",
-    "Do you often think about the future and possibilities?",
-    "Do you make decisions based on logic and analysis?",
-    "Do you enjoy social interactions and meeting new people?",
-    "Do you value harmony and strive to avoid conflicts?",
-    "Are you often introspective and reflective?",
-    "Do you trust your intuition when making decisions?"
+    "Do you prefer socializing with others? (Yes/No)",
+    "Are you detail-oriented and focused on the present? (Yes/No)",
+    "Do you make decisions based on logic and analysis? (Yes/No)",
+    "Are you organized and like to plan ahead? (Yes/No)"
 ]
 
 # Loop through questions and get user responses iteratively
-for i in range(8):
+for i in range(4):
     if i == current_question:
         st.write(f"Question {i + 1}:")
-        response = st.text_input(f"{questions[i]} (Yes/No)", key=f"question_{i}")
+        response = st.text_input(f"{questions[i]} (Yes/No) (Press Enter for the next question)", key=f"question_{i}")
         response = response.strip().lower()  # Convert to lowercase and remove leading/trailing spaces
         if response == 'yes' or response == 'no':
             responses.append(response)
@@ -105,18 +95,47 @@ for i in range(8):
             st.warning("Re-enter answer please")  # Display error message for invalid response
 
 # Assess personality based on responses
-personality = "ENFP"  # You can change this to the actual assessment function based on responses
+# Assess personality based on responses
+personality = ""
 
-# Check if all 8 questions have been answered
-if current_question == 8:
-    # Suggest a country and places using ChatGPT
-    suggested_country_and_places = suggest_country_and_places_from_chatgpt(personality)
+# Check if there are enough responses and user responses to determine personality
+if len(responses) >= 4:
+    if responses[0] == 'yes':
+        personality += "E"
+    else:
+        personality += "I"
+
+    if responses[1] == 'yes':
+        personality += "S"
+    else:
+        personality += "N"
+
+    if responses[2] == 'yes':
+        personality += "T"
+    else:
+        personality += "F"
+
+    if responses[3] == 'yes':
+        personality += "J"
+    else:
+        personality += "P"
+else:
+    st.warning("Please answer all 4 questions to determine personality.")
+
+# Suggest a country and places using ChatGPT
+suggested_country_and_places = suggest_country_and_places_from_chatgpt(personality)
+
+# Check if all 4 questions have been answered
+# Check if all 4 questions have been answered
+if current_question == 4:
     if suggested_country_and_places:
         st.subheader("Suggested Country and Places in Saudi Arabia:")
-        st.write(f"Country: {suggested_country_and_places[0]}")
-        st.write("Suggested Places:")
-        for place in suggested_country_and_places[1:]:
-            st.write(f"- {place}")
+        country = suggested_country_and_places[0]
+        places = suggested_country_and_places[1:]
+
+        st.write(f"{country} :")
+        for place in places:
+            st.write(f"  * {place}")
     else:
         st.warning("Sorry, no suggestions available for this personality.")
 
